fdb45563fe8e93a62c1a95d9280416f8ca7ed622
Chi Hieu <chihieuwe12@gmail.com>
2023-07-10 22:24:36 +0700

    Adding images, change page layout, eliminate caching, modify text font, add new images

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template 
 
 app = Flask(__name__)
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    response.headers['Surrogate-Control'] = 'no-store'
+    return response
 
 @app.route("/")
 def index():
