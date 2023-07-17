ac01840fdd5140990873e183d42f1e40dda2d874
Chi Hieu <chihieuwe12@gmail.com>
2023-07-17 21:40:57 +0700

    Modify navbar, fix navbar width view dropdown, add libraries and session

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, flash, session, redirect
+from flask_session import Session
 from cs50 import SQL
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -27,10 +35,13 @@ def login():
     else: 
         return render_template("login.html", navbar_style='navbar-alt', navbar_brand_style='navbar-brand-alt', nav_link_style='nav-link-alt')
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    email = request.form.get("email")
+    password = request.form.get("confirmation")
     if request.method == "POST":
-        ...
+        if '@' not in email:
+            flash("Email not valid")
+        return redirect("/register")
     else:
         return render_template("register.html", navbar_style='navbar-alt', navbar_brand_style='navbar-brand-alt', nav_link_style='nav-link-alt')
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
     var scrolled = false; // Variable to track scroll state
     $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        var navbar = $(".navbar");
-        var navlink = $(".nav-link");
-        var navbarBrand = $(".navbar-brand");
+        let scroll = $(window).scrollTop();
+        let navbar = $(".navbar");
+        let navlink = $(".nav-link");
+        let navbarBrand = $(".navbar-brand");
+        let credential = $(".credential");
         if (scroll > 300) {
             if (!scrolled) {
                 navbar.css("background", "#FFC0CB");
                 navlink.css("color", "black");
                 navbarBrand.css("color", "black");
+                credential.css("color", "black");
                 scrolled = true;
             }
         } else {
@@ -17,6 +19,7 @@ $(document).ready(function(){
                 navbar.css("background", "transparent");
                 navlink.css("color", "rgb(255, 189, 189)");
                 navbarBrand.css("color", "rgb(255, 189, 189)");
+                credential.css("color", "rgb(255, 189, 189)");
                 scrolled = false;
             }
         }
