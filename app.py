7eeded75e4d1322d9294e8160b5db3bfbe23d8e1
chihieuwe <46745645+chihieuwe@users.noreply.github.com>
2023-07-19 05:54:05 +0000

    Add validation and error messages for register form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, session, redirect
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
 app = Flask(__name__)
 
@@ -37,11 +38,29 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    email = request.form.get("email")
-    password = request.form.get("confirmation")
     if request.method == "POST":
-        if '@' not in email:
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("confirmation")
+        checkmail = db.execute("SELECT * FROM users WHERE email = ?", email)
+        if not name:
+            flash("Name cannot be blank")
+            return redirect("/register")
+        if not email:
+            flash("Email cannot be blank")
+            return redirect("/register")
+        if not password:
+            flash("Password cannot be blank")
+            return redirect("/register")
+        if '@' not in email or email.isnumeric == True:
             flash("Email not valid")
+            return redirect("/register")
+        if checkmail:
+            flash("Email already exists!")
+            return redirect("/register")
+        else:
+            db.execute("INSERT INTO users (name, email, hash) VALUES (?, ?, ?)", name, email, generate_password_hash(password))
+            flash("Registered successfully!")
         return redirect("/register")
     else:
         return render_template("register.html", navbar_style='navbar-alt', navbar_brand_style='navbar-brand-alt', nav_link_style='nav-link-alt')
